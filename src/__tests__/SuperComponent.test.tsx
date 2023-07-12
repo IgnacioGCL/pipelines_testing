5c9ba49a6053719c4b2542d25f6996a9767b0ee3
Nacho García <nacho@treend.es>
2023-07-12 01:21:28 +0100

    Add tests and github actions

--- src/__tests__/SuperComponent.test.tsx
+++ src/__tests__/SuperComponent.test.tsx
@@ -14,4 +14,26 @@ describe("SuperComponent", () => {
     fireEvent.click(button);
     expect(screen.getByRole("button")).toHaveTextContent("count is 1");
   });
+
+  it("should have red color when counter is even", () => {
+    render(<SuperComponent />);
+    const counter = screen.getByTestId("counter");
+    expect(counter.style.color).toBe("red");
+    const button = screen.getByRole("button");
+    fireEvent.click(button);
+    expect(counter.style.color).not.toBe("red");
+    fireEvent.click(button);
+    expect(counter.style.color).toBe("red");
+  });
+
+  it("should have blue color when counter is odd", () => {
+    render(<SuperComponent />);
+    const counter = screen.getByTestId("counter");
+    expect(counter.style.color).not.toBe("blue");
+    const button = screen.getByRole("button");
+    fireEvent.click(button);
+    expect(counter.style.color).toBe("blue");
+    fireEvent.click(button);
+    expect(counter.style.color).not.toBe("blue");
+  });
 });
--- src/components/SuperComponent.tsx
+++ src/components/SuperComponent.tsx
@@ -1,10 +1,20 @@
-import { FC, useState } from "react";
+import { CSSProperties, FC, useState } from "react";
 
 const SuperComponent: FC = () => {
   const [count, setCount] = useState(0);
   const increaseCounter = () => setCount((count) => count + 1);
 
-  return <button onClick={increaseCounter}>count is {count}</button>;
+  const isEven = count % 2 === 0;
+
+  const countColor: CSSProperties = {
+    color: isEven ? "red" : "blue",
+  };
+
+  return (
+    <button onClick={increaseCounter}>
+      count is <span data-testid="counter" style={countColor}>{count}</span>
+    </button>
+  );
 };
 
 export default SuperComponent;
